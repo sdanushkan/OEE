bc4614343b3ab9766271f1516f6d2d466462074a
danushkan2002 <105484594+danushkan2002@users.noreply.github.com>
2023-08-20 20:10:01 +0530

    dubug false

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m%^3a)*le!=vbzv$25s_xmqw+#ku-i)7ymjvorep1u3n3)0l(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["srk-production.up.railway.app", "www.srk-elite.com", "srk.elite.com"]
 
