2d768a3a41a37833e68eadd906a87d5e526bf12e
danushkan2002 <105484594+danushkan2002@users.noreply.github.com>
2023-10-23 08:58:56 -0700

    admin hide

--- backend/urls.py
+++ backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('', TemplateView.as_view(template_name='index.html')),
